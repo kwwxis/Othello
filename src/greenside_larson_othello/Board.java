8d631a623fa525ec043073b5bc0325e4ee82722a
tmgreenside <trevmg7@gmail.com>
2018-04-09 19:20:00 -0700

    Added row and column labels for board

--- src/greenside_larson_othello/Board.java
+++ src/greenside_larson_othello/Board.java
@@ -5,6 +5,7 @@
  */
 package greenside_larson_othello;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 
@@ -14,18 +15,10 @@ import javafx.scene.shape.Rectangle;
  */
 public class Board extends GridPane {
     
-    // private Space[][] spaces;
     private static int DIMENSION;
     private final Rectangle[][] spaces;
     
     public Board(int numSpaces) {
-//        DIMENSION = numSpaces;
-//        spaces = new Space[DIMENSION][DIMENSION];
-//        this.setHeight(600);
-//        this.setWidth(600);
-//        this.setStyle("-fx-background-color: green;");
-//        addSpaces();
-
         DIMENSION = numSpaces;
         spaces = new Rectangle[DIMENSION][DIMENSION];
         this.setHeight(800);
@@ -35,11 +28,32 @@ public class Board extends GridPane {
     
     @SuppressWarnings("static-access")
     private void addSpaces() {
+        
+        // create row and column labels
+        // row labels:
+        for (int i = 0; i < DIMENSION; i++) {
+            String rowName = Character.toString((char) (i + 49));
+            Label rowLabel = new Label(rowName);
+            this.setRowIndex(rowLabel, (i+1));
+            this.setColumnIndex(rowLabel, 0);
+            this.getChildren().add(rowLabel);
+        }
+        
+        // column labels:
+        for (int i = 0; i < DIMENSION; i++) {
+            String rowName = Character.toString((char) (i + 65));
+            Label rowLabel = new Label(rowName);
+            this.setRowIndex(rowLabel, 0);
+            this.setColumnIndex(rowLabel, (i+1));
+            this.getChildren().add(rowLabel);
+        }
+        
+        // create squares
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
                 spaces[i][j] = new Space();
-                this.setRowIndex(spaces[i][j], i);
-                this.setColumnIndex(spaces[i][j], j);
+                this.setRowIndex(spaces[i][j], i+1);
+                this.setColumnIndex(spaces[i][j], j+1);
                 this.getChildren().add(spaces[i][j]);
             }
         }
