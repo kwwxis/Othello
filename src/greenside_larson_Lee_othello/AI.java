ea60b154203cca9911c5f09b91f9cd6aefe19bab
tmgreenside <trevmg7@gmail.com>
2018-05-01 17:08:57 -0700

    Added private AI Timer class

--- src/greenside_larson_Lee_othello/AI.java
+++ src/greenside_larson_Lee_othello/AI.java
@@ -5,6 +5,11 @@
  */
 package greenside_larson_Lee_othello;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+
 /**
  *
  * @author Trevor Greenside
@@ -18,4 +23,34 @@ public class AI {
         game.initBoard();
     }
     
+    // AI turn, make a move
+    public void moveAI() {
+        // create timer in a new thread
+        
+        // get board state // see update state
+        
+        // build tree
+    }
+    
+    // stop AI if timer reaches end
+    public void stopAI() {
+        
+    }
+    
+    private class AITimer {
+        
+        private final Timeline decrementer;
+        
+        public AITimer(AI ai) {
+            decrementer = new Timeline(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> {
+                timeDecrement();
+            }));
+            decrementer.setCycleCount(Timeline.INDEFINITE);
+            decrementer.play();
+        }
+        
+        public void timeDecrement() {
+            // TODO
+        }
+    }
 }
